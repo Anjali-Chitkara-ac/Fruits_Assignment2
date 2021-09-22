122293e62e23651183f7d0abfa99686b6c29de9f
Anjali-Chitkara-ac <69366199+Anjali-Chitkara-ac@users.noreply.github.com>
2021-09-21 23:01:53 -0700

    Initial Commit

---
+++ Fruits_Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fruits_Assignment2
+//
+//  Created by Anjali Chitkara on 9/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
